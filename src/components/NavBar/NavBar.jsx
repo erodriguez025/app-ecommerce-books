e12fed21511198c9fcaa409e9203deef19f4b121
Juan Rodriguez <emirodriguez_025@hotmail.com>
2022-06-01 16:10:08 -0300

    customización navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,26 +5,33 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import ebook from './ebook.png'
+
+
 
 export default function NavBar() {
+
+
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="fixed" style={{backgroundColor: "#354259"}}>
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <img 
+                src={ebook}
+                alt='Comerce.js'
+                height='50px'
+            />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+            eBooks
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Hello guest
+          </Typography>
+          <Button variant="outlined">
+            <strong>Sign In</strong>
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
